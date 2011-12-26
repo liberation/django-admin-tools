c3b73686856b5e6090f9a5cec9a613c3eeb489eb
Souen Boniface <souenboniface@gmail.com>
2011-12-26 20:18:35 +0100

    Multi AdminSite() support for menu
    TODO:
    - DRYify new util _get_menu_cls (see dashboard.util._get_dashboard_cls)
    - Use `current_app` kwargs when reversing AdminSite() URLs
    - Need tests!

--- admin_tools/menu/utils.py
+++ admin_tools/menu/utils.py
@@ -1,21 +1,40 @@
 """
 Menu utilities.
 """
+import types
 
 from django.conf import settings
+from django.core.urlresolvers import reverse
 from django.core.exceptions import ImproperlyConfigured
 from django.utils.importlib import import_module
 
 
-def get_admin_menu():
+def get_admin_menu(context):
     """
     Returns the admin menu defined by the user or the default one.
     """
-    menu_cls = getattr(
+    return _get_menu_cls(getattr(
         settings,
         'ADMIN_TOOLS_MENU',
         'admin_tools.menu.DefaultMenu'
-    )
-    mod, inst = menu_cls.rsplit('.', 1)
-    mod = import_module(mod)
-    return getattr(mod, inst)()
+    ), context)()
+
+
+# FIXME Copied from admin_tools.dashboard.utils._get_dashboard_cls... DRY alert!
+def _get_menu_cls(menu_cls, context):
+    if type(menu_cls) is types.DictType:
+        curr_url = context.get('request').META['PATH_INFO']
+        for key in menu_cls:
+            admin_site_mod, admin_site_inst = key.rsplit('.', 1)
+            admin_site_mod = import_module(admin_site_mod)
+            admin_site = getattr(admin_site_mod, admin_site_inst)
+            admin_url = reverse('%s:index' % admin_site.name)  # FIXME use current_app to reverse custom AdminSite!
+            if curr_url.startswith(admin_url):
+                mod, inst = menu_cls[key].rsplit('.', 1)
+                mod = import_module(mod)
+                return getattr(mod, inst)
+    else:
+        mod, inst = menu_cls.rsplit('.', 1)
+        mod = import_module(mod)
+        return getattr(mod, inst)
+    raise ValueError('Menu matching "%s" not found' % dashboard_cls)
