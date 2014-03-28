c3b47e51d687bded1ce7bdaa1fdc871c11a1d8cb
Fantomas42 <fantomas42@gmail.com>
2014-03-28 11:18:17 +0100

    Compatibility for Django >= 1.5

--- admin_tools/dashboard/urls.py
+++ admin_tools/dashboard/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import patterns, url
+from django.conf.urls import patterns, url
 
 urlpatterns = patterns('admin_tools.dashboard.views',
     url(r'^set_preferences/(?P<dashboard_id>.+)/$', 'set_preferences', name='admin-tools-dashboard-set-preferences'),
